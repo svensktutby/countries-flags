1c6373660b1947d2c9eb90ecce475c74bbcf098a
Andrei Shved <svensk.tut.by@gmail.com>
2022-06-06 21:10:01 +0300

    feat: add persisted local storage theme

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
+import throttle from 'lodash.throttle';
 
 import { rootReducer } from './root-reducer';
 import * as api from '../config';
+import { loadState, saveState } from '../utils';
+
+const persistedState = loadState();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
+    persistedState,
     composeEnhancers(
         applyMiddleware(
             thunk.withExtraArgument({
@@ -17,4 +22,12 @@ export const store = createStore(
             }),
         ),
     ),
+);
+
+store.subscribe(
+    throttle(() => {
+        saveState({
+            theme: store.getState().theme,
+        });
+    }, 1000),
 );
